0f842bb92296e2121f399b8423c757917a55c21a
Kieran Griffiths <griffk04@gmail.com>
2021-10-20 23:05:34 +0800

    Removed a bunch of dead 2d code

--- client/src/app_state.rs
+++ client/src/app_state.rs
@@ -4,14 +4,6 @@ pub struct Canvas {
     pub height: f32,
 }
 
-#[derive(Debug, Clone, Copy, Default, PartialEq)]
-pub struct Control {
-    pub bottom: f32,
-    pub top: f32,
-    pub left: f32,
-    pub right: f32,
-}
-
 #[derive(Debug, Clone, Copy, Default, PartialEq, PartialOrd)]
 pub struct Angles {
     pub x: f32,
@@ -22,7 +14,6 @@ pub struct Angles {
 #[derive(Debug, Default, Clone)]
 pub struct AppState {
     pub canvas: Canvas,
-    pub control: Control,
     pub angles: Angles,
     pub time: f32,
 }
@@ -39,23 +30,9 @@ impl AppState {
     }
 
     pub fn update(&mut self, time: f32, canvas: Canvas) {
-        let min_height_width = canvas.height.min(canvas.width);
-        let display_size = 0.9 * min_height_width;
-
-        let half_display_size = display_size * 0.5;
-        let half_canvas_width = canvas.width * 0.5;
-        let half_canvas_height = canvas.height * 0.5;
-
         self.canvas = canvas;
         let time_step = time - self.time;
         self.time = time;
-
-        self.control = Control {
-            bottom: half_canvas_height - half_display_size,
-            top: half_canvas_height + half_display_size,
-            left: half_canvas_width - half_display_size,
-            right: half_canvas_width + half_display_size,
-        };
 
         self.angles.x += 0.00037 * time_step;
         self.angles.y += 0.00059 * time_step;
--- client/src/shaders.rs
+++ client/src/shaders.rs
@@ -1,2 +1,1 @@
-pub mod color2d_gradient;
 pub mod cube;
