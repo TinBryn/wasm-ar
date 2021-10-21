020f7d1c7a3cd8433fecfb57537037fc578819a8
Kieran Griffiths <20491633@student.uwa.edu.au>
2021-10-21 09:08:43 +0800

    Added console_log macro

--- client/src/utils.rs
+++ client/src/utils.rs
@@ -17,3 +17,10 @@ extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
 }
+
+#[macro_export]
+
+macro_rules! console_log {
+    ($($t:tt)*) =>
+        (crate::utils::log(&format_args!($($t)*).to_string()))
+}
